d47969ebd23edc2f28e291712a8f93e5037a1755
ayushi <ayushi8855@gmail.com>
2023-01-15 09:57:04 +0530

    json done

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,11 +1,21 @@
-import { Box, Button, FormControl, FormLabel, Input, Select, Text } from '@chakra-ui/react'
+import { Box, Button, ButtonGroup, FormControl, FormLabel, Input, Popover, PopoverArrow, PopoverBody, PopoverCloseButton, PopoverContent, PopoverFooter, PopoverHeader, PopoverTrigger, Select, useDisclosure} from '@chakra-ui/react'
+import axios from 'axios'
+import { nanoid } from 'nanoid'
 import React, { useState } from 'react'
 import Nav from './Nav'
 
 const Product = () => {
-
+    const { isOpen, onToggle, onClose } = useDisclosure()
     const [files, setFiles] = useState([]);
-    const[data,setdata]=useState({})
+    const[data,setdata]=useState({
+      "id":nanoid(4),
+      "name":"",
+      "description":" ",
+      "category":"",
+      "img":"",
+      "partNumber":"",
+      "size":"2"
+    })
     const [image,setimage]=useState()
 
   const handleDrop = (e) => {
@@ -29,7 +39,23 @@ const Product = () => {
      setdata({
       ...data,
       [name]: value,
+      img:image
      });
+     
+  }
+
+  async function handleSubmit(){
+    try {
+        let res = await axios.post("http://localhost:8080/product",data)
+        
+        alert("Add successfull !");
+        
+      } catch (error) {
+        alert(error.response.data.message);
+        console.log(error);
+      }
+  
+      console.log("details",data);
   }
 
   return (
@@ -40,7 +66,7 @@ const Product = () => {
        <FormLabel>Create Product</FormLabel>
        <Input placeholder='Product Name' onChange={handleChange} name="name"/>
        <FormLabel></FormLabel>
-       <Input placeholder='Product Description' onChange={handleChange} name="Description" />
+       <Input placeholder='Product Description' onChange={handleChange} name="description" />
        <FormLabel></FormLabel>
        <Select placeholder='Select Category' onChange={handleChange} name="category">
        <option value="Category 1">Category 1</option>
@@ -52,12 +78,12 @@ const Product = () => {
       onDragOver={(e) => e.preventDefault()}
       style={{width:"150%",  height: '300px', border: '2px solid #ECECEC' ,display:"flex",justifyContent:"space-between" ,marginTop:"50px"}}
     >
-        <Box w={"50%"} m={2} border={"1px solid #ECECEC"} p={3} h={200}>
+        <Box w={"50%"} m={2} border={"2px solid #ECECEC"} p={3} h={200} borderStyle="dashed">
         <input type="file" onChange={handleInputChange}  />
         <p align="center">Drag And Drop</p> 
         </Box>
      
-      <Box border={"1px solid #ECECEC"} w={"50%"} m={2} p={3} >
+      <Box border={"2px solid #ECECEC"} w={"50%"} m={2} p={3} borderStyle="dashed" >
         {files.map((file, index) => (
           <div key={index}>{file.name}</div>
          
@@ -65,9 +91,28 @@ const Product = () => {
       
      </Box>
       </Box>
-      <Button border="1px solid #0081C9" bg={"white"} color={"#0081C9"} mt={3}>Add Product Detail</Button>
+
+    
+      <Popover>
+  <PopoverTrigger>
+   
+      <Button border="1px solid #0081C9" bg={"white"} color={"#0081C9"} mt={3} onClick={onToggle}>Add Product Detail</Button>
+  </PopoverTrigger>
+  <PopoverContent  >
+  <PopoverHeader fontWeight='semibold'>Add Product Detail</PopoverHeader>
+    <PopoverArrow bg='#0081C9' />
+    <PopoverCloseButton bg='#0081C9' />
+    {/* <PopoverBody>
+      Tadaa!! The arrow color and background color is customized. Check the
+      props for each component.
+   
+    </PopoverBody> */} 
+    <Input placeholder='ADD PART NO.'name='partNumber' onChange={handleChange}></Input>
+    <Input placeholder='Add Size' name="size" onChange={handleChange}></Input>
+   </PopoverContent>
+   </Popover>
       <br></br>
-      <Button  bg={"#0081C9"} color={"white"} mt={3}>Submit</Button>
+      <Button  bg={"#0081C9"} color={"white"} mt={3} onClick={handleSubmit}>Submit</Button>
      
         </FormControl>
 
